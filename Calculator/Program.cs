759ca5113d49bbec202b29cd9818e745b4103c94
Gustayz <114109045+Gustayz@users.noreply.github.com>
2023-12-31 16:19:00 -0500

    Update: ask the user to input two whole numbers and the program will store them in a variable called userNumber.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -9,20 +9,30 @@ namespace Calculator // Note: actual namespace depends on the project name.
 
             Console.WriteLine("Hi there, whats your name?");
             string userName = Convert.ToString(Console.ReadLine());
-            Console.WriteLine($"Hello {userName}, choice an option down below.");
+            Console.WriteLine($"Hello {userName}, choose an option down below. (type a number 1-4) ");
             Menu();
+            int userChoice = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter two whole numbers (separate them with space)");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
         }
 
         static void Menu()
         {
-            Console.WriteLine(" ONLINE CALCULATOR");
-            Console.WriteLine("*******************");
-            Console.WriteLine("*                 *");
-            Console.WriteLine("*                 *");
-            Console.WriteLine("*                 *");
-            Console.WriteLine("*                 *");
-            Console.WriteLine("*                 *");
-            Console.WriteLine("*******************");
+            Console.WriteLine(" ---ONLINE CALCULATOR---");
+            Console.WriteLine("  *******************");
+            Console.WriteLine("  * 1. Multiply     *");
+            Console.WriteLine("  * 2. divide       *");
+            Console.WriteLine("  * 3. Add          *");
+            Console.WriteLine("  * 4. Subtract     *");
+            Console.WriteLine("  *******************");
+        }
+
+        static int Choice(int number)
+        {
+           
+
+            return number;
         }
     }
 }
