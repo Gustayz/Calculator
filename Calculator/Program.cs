bdf5d83f91ff0273f73cba7b2f896c2422bdcb98
Gustayz <114109045+Gustayz@users.noreply.github.com>
2023-12-31 11:06:47 -0500

    Update: created an introduction to the calculater by asking the user for its name and then storing the data in a variable, so that later can be used.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,7 +6,23 @@ namespace Calculator // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hi there, whats your name?");
+            string userName = Convert.ToString(Console.ReadLine());
+            Console.WriteLine($"Hello {userName}, choice an option down below.");
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.WriteLine(" ONLINE CALCULATOR");
+            Console.WriteLine("*******************");
+            Console.WriteLine("*                 *");
+            Console.WriteLine("*                 *");
+            Console.WriteLine("*                 *");
+            Console.WriteLine("*                 *");
+            Console.WriteLine("*                 *");
+            Console.WriteLine("*******************");
         }
     }
 }
