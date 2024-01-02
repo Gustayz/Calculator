a52cbea92c0630977cc6a9f13db1af3fdd5333a8
Gustayz <114109045+Gustayz@users.noreply.github.com>
2024-01-01 19:07:13 -0500

    Update: less complex and more readable. With comments and a new method called FinalResult.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.ComponentModel.Design;
+using System.Linq.Expressions;
+using System.Reflection.Metadata;
+using System.Runtime.ConstrainedExecution;
 
 namespace Calculator // Note: actual namespace depends on the project name.
 {
@@ -6,33 +10,55 @@ namespace Calculator // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
+            int userNum1, userChoice, userNum2;
 
-            Console.WriteLine("Hi there, whats your name?");
-            string userName = Convert.ToString(Console.ReadLine());
-            Console.WriteLine($"Hello {userName}, choose an option down below. (type a number 1-4) ");
+            // getting the user name
+            Console.WriteLine("Hello there, whats your name?");
+            string userName = Console.ReadLine();
+
+            // getting the user's two whole numbers
+            Console.WriteLine("Before we continue, please enter first number: ");
+            userNum1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Now enter the second number: ");
+            userNum2 = Convert.ToInt32(Console.ReadLine());
+
+            // getting the user choice
             Menu();
-            int userChoice = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please enter two whole numbers (separate them with space)");
-            int userNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Welcome {userName}, please choose an option above. (Only numbers 1-4)");
+            userChoice = Convert.ToInt32(Console.ReadLine());
 
+             int userFinalResult = FinalResult(userChoice, userNum1, userNum2);
         }
 
         static void Menu()
         {
-            Console.WriteLine(" ---ONLINE CALCULATOR---");
-            Console.WriteLine("  *******************");
-            Console.WriteLine("  * 1. Multiply     *");
-            Console.WriteLine("  * 2. divide       *");
-            Console.WriteLine("  * 3. Add          *");
-            Console.WriteLine("  * 4. Subtract     *");
-            Console.WriteLine("  *******************");
+            Console.WriteLine("ONLINE CALCULATOR");
+            Console.WriteLine("****************");
+            Console.WriteLine("* 1. Multiply  *");
+            Console.WriteLine("* 2. Divide    *");
+            Console.WriteLine("* 3. Add       *");
+            Console.WriteLine("* 4. Subtract  *");
+            Console.WriteLine("****************");
         }
 
-        static int Choice(int number)
+        static int FinalResult(int userChoice, int userNum)
         {
-           
+            int final;
 
-            return number;
+            switch (userChoice)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                default:
+                    break;
+            }
+            return final;
         }
     }
 }
